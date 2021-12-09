05ac6f80b1dede470873f78e51d10f9470a973f6
Hitesh <hiteshspatil99@gmail.com>
2021-12-09 08:23:15 +0530

    [Hitesh] add : Third Party Library Using CSV file

---
+++ ThirdPartyLibrary/ThirdPartyLibrary/CsvHandler.cs
@@ -0,0 +1,49 @@
+﻿using CsvHelper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ThirdPartyLibrary
+{
+    public class CsvHandler
+    {
+
+        public static void ImplementCSVdatHandling()
+        {
+            string importFilePath = @"D:\third party\ThirdPartyLibrary\ThirdPartyLibrary\Utility\addresses.csv";
+            string exportFilePath = @"D:\third party\ThirdPartyLibrary\ThirdPartyLibrary\Utility\export.csv";
+
+            using (var reader = new StreamReader(importFilePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<AddressData>().ToList();
+                Console.WriteLine("Read data succesfully fromaddresses csv");
+                foreach (AddressData addressData in records)
+
+                {
+                    Console.WriteLine(addressData.firstname);
+                    Console.WriteLine(addressData.lastname);
+                    Console.WriteLine(addressData.address);
+                    Console.WriteLine(addressData.city);
+                    Console.WriteLine(addressData.state);
+                    Console.WriteLine("\t" + addressData.code);
+                    Console.Write("\n");
+                }
+
+                Console.WriteLine("\n  now reading from csv file & write to csv file");
+
+                using (var writer = new StreamWriter(exportFilePath))
+                using (var CsvExport = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    CsvExport.WriteRecords(records);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ThirdPartyLibrary/ThirdPartyLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ThirdPartyLibrary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("# Read data from CSV & Write data in CSv");
+            CsvHandler.ImplementCSVdatHandling();
+            Console.WriteLine();
+
+        }
+
+    }
+}
